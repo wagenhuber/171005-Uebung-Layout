70602184705a20b4d451ff99d93edb3991ac2dd7
wagenhuber <wagenhuber@gmx.net>
2017-10-12 09:26:02 +0200

    Combobox hinzugefügt

--- src/com/sabel/Fenster.java
+++ src/com/sabel/Fenster.java
@@ -8,24 +8,33 @@ import java.awt.event.ActionListener;
 
 public class Fenster extends JFrame {
 
-    private JPanel panelWest, panelSouth, panelCenter;
+    private JPanel panelWest, panelSouth, panelCenter, panelNorth;
     private JRadioButton radioButtonRed, radioButtonGreen, radioButtonBlue;
     private ButtonGroup radioButtonGroup;
     private JButton buttonRed, buttonGreen, buttonBlue;
+    private String[] itemsForComboBox = {"Rot", "Grün", "Blau"};
+    private JComboBox<String> comboBox;
 
 
     public Fenster() {
         super("Übung Layout");
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        this.setSize(600, 600);
+//        this.setSize(600, 600);
         initComponets();
+        this.pack();
         this.setVisible(true);
 
     }
 
     private void initComponets() {
+
+        //JPanels erzeugen
+
         panelWest = new JPanel();
         panelWest.setLayout(new BoxLayout(panelWest, BoxLayout.Y_AXIS));
+        panelCenter = new JPanel();
+        panelSouth = new JPanel();
+        panelNorth = new JPanel();
 
 
         //JRadioButtons erzeugen
@@ -42,16 +51,16 @@ public class Fenster extends JFrame {
         radioButtonGroup.add(radioButtonBlue);
 
 
+        //JCombox erzeugen
+        comboBox = new JComboBox<>(itemsForComboBox);
+
+        //Combobox zu JPanel north hinzufügen
+        panelNorth.add(comboBox);
+
         //JRadioButtons zu Panel hinzufügen
         panelWest.add(radioButtonRed);
         panelWest.add(radioButtonGreen);
         panelWest.add(radioButtonBlue);
-
-
-
-        //JPanels erzeugen
-        panelCenter = new JPanel();
-        panelSouth = new JPanel();
 
 
         //JButtons erzeugen
@@ -70,6 +79,7 @@ public class Fenster extends JFrame {
         this.add(panelWest, BorderLayout.WEST);
         this.add(panelCenter);
         this.add(panelSouth, BorderLayout.SOUTH);
+        this.add(panelNorth, BorderLayout.NORTH);
 
 
         //SET BORDER
@@ -78,7 +88,7 @@ public class Fenster extends JFrame {
         panelCenter.setBorder(new TitledBorder(""));
 
 
-        //Event handling
+        //Event handling für RadioButtons
         radioButtonRed.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -102,7 +112,69 @@ public class Fenster extends JFrame {
             }
         });
 
+
+        //Eventhandling für Combobox
+        comboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                switch ((String) comboBox.getSelectedItem()) {//Methode auf Rückgabewert String gecastet
+                    case "Rot":
+                        panelCenter.setBackground(Color.RED);
+                        comboBox.setSelectedItem("Rot");
+                        radioButtonRed.setSelected(true);
+                        break;
+                    case "Grün":
+                        panelCenter.setBackground(Color.GREEN);
+                        comboBox.setSelectedItem("Grün");
+                        radioButtonGreen.setSelected(true);
+                        break;
+                    case "Blau":
+                        panelCenter.setBackground(Color.BLUE);
+                        comboBox.setSelectedItem("Blau");
+                        radioButtonBlue.setSelected(true);
+                        break;
+
+                }
+            }
+        });
+
+
+//Eventhandling für Buttons
+
+        meinInnererActionListener mial = new meinInnererActionListener();
+        buttonRed.addActionListener(mial);
+        buttonGreen.addActionListener(mial);
+        buttonBlue.addActionListener(mial);
+
     }
+
+
+    public class meinInnererActionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.out.println(e.getActionCommand());
+            switch (e.getActionCommand()) {
+                case "Rot":
+                    panelCenter.setBackground(Color.RED);
+                    comboBox.setSelectedItem("Rot");
+                    radioButtonRed.setSelected(true);
+                    break;
+                case "Grün":
+                    panelCenter.setBackground(Color.GREEN);
+                    comboBox.setSelectedItem("Grün");
+                    radioButtonGreen.setSelected(true);
+                    break;
+                case "Blau":
+                    panelCenter.setBackground(Color.BLUE);
+                    comboBox.setSelectedItem("Blau");
+                    radioButtonBlue.setSelected(true);
+                    break;
+            }
+        }
+
+    }
+
 
     public static void main(String[] args) {
         new Fenster();
